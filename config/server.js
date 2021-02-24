0b2b867ab2a222ebf163dbd68d146eb44b66a272
berniedev007 <berniedev007@gmail.com>
2021-02-24 02:00:08 -0500

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8054c26175f2cbf9a44232082072b04d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '32409428-046c-4414-8274-61f1ddf98375'
+};
